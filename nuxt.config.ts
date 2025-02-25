db13f20e60bd531552dd5261b4b724a39bd2ca99
David Castillo <vikruzdavid@gmail.com>
2025-02-25 14:08:24 -0400

    Final Config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@pinia/nuxt', '@nuxt/eslint', 'pinia-plugin-persistedstate/nuxt'],
+  modules: ['nitro-cloudflare-dev', '@nuxt/eslint', '@nuxt/ui'],
   components: [
     {
       path: '~/components/atoms',
@@ -11,6 +11,6 @@ export default defineNuxtConfig({
   css: ['~/assets/scss/styles.scss'],
   compatibilityDate: '2024-11-01',
   nitro: {
-    preset: 'cloudflare-pages',
+    preset: 'cloudflare_pages',
   },
 });
