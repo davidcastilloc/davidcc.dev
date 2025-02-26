7a1563eeceb03e5df724c8c87281378e26ff04d7
David Castillo <vikruzdavid@gmail.com>
2025-02-26 02:09:49 -0400

    Add Modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['nitro-cloudflare-dev', '@nuxt/eslint', '@nuxt/ui'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/ui',
+    '@nuxt/content',
+    '@nuxt/fonts',
+    '@nuxtjs/color-mode',
+    '@vueuse/nuxt',
+    '@pinia/nuxt',
+    'pinia-plugin-persistedstate',
+    'nitro-cloudflare-dev',
+  ],
   devtools: { enabled: false },
   css: ['~/assets/scss/styles.scss'],
   compatibilityDate: '2024-11-01',
