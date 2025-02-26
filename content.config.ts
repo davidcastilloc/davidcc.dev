e2ff9232618dd2eea7d9f65fcad670309f2fc572
David Castillo <vikruzdavid@gmail.com>
2025-02-26 04:26:40 -0400

    Add content

--- content.config.ts
+++ content.config.ts
@@ -1,10 +1,15 @@
 import { defineContentConfig, defineCollection } from '@nuxt/content';
-
+import { schemaResume } from './content/models/resume'
 export default defineContentConfig({
   collections: {
     content: defineCollection({
       type: 'page',
-      source: '**/*.md',
+      source: 'works/**/*.md',
+    }),
+    resume: defineCollection({
+      type: 'data',
+      source: 'resumes/**/*.json',
+      schema: schemaResume,
     }),
   },
 });
